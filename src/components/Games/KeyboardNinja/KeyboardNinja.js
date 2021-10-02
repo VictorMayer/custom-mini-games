31c7b756c39e9f774fd6708cc0da02e2737b55b7
Victor Mayer <victor.mayer@outlook.com>
2021-10-01 23:58:17 -0300

    initial project configured, sketch of main menu created

---
+++ src/components/Games/KeyboardNinja/KeyboardNinja.js
@@ -0,0 +1,7 @@
+export default function KeyboardNinja() {
+    return (
+        <>
+        this will be a mini-game
+        </>
+    )
+}
---
+++ src/components/Home.js
@@ -0,0 +1,77 @@
+import styled from "styled-components";
+import { useHistory } from "react-router";
+
+export default function Home() {
+
+    const history = useHistory();
+
+    function goToGame(game) {
+        console.log(game);
+        switch (game) {
+            case 1: history.push("/keyboard-typer-ninja");break;
+            default: break;
+        }
+    }
+
+    return (
+        <HomeContainer>
+            <Logo>
+                Custom Mini-Games
+            </Logo>
+            <MenuContainer>
+                <p className="game-opt" onClick={()=>goToGame(1)} >keyboard typer ninja</p>
+                <p className="game-opt" onClick={()=>goToGame(2)} >Cursor Dodger</p>
+                <p className="game-opt" onClick={()=>goToGame(3)} >Aimbot Trainer</p>
+            </MenuContainer>
+        </HomeContainer>
+    )
+}
+
+const MenuContainer=styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+background: radial-gradient(#eee, #ccc);
+border-radius: 15px;
+min-height: 700px;
+min-width:500px;
+font-weight:700;
+font-size: 30px;
+.game-opt{
+    text-align: center;
+    padding: 2px 15px;
+    width: 100%;
+    cursor: default;
+    &::selection{
+    background-color:#F149C9;
+    color:#fff;
+    }
+    &:hover{
+    font-size: 40px;
+    transition: 500ms;
+    }
+}
+`
+
+const Logo = styled.div`
+margin-top: 25px;
+margin-bottom:75px;
+font-weight: 700;
+font-size: 70px;
+color:#111;
+text-align: center;
+&::selection{
+    background-color:#f3aca9;
+    color:#fff;
+    }
+&:hover{
+    font-size: 85px;
+    transition: 500ms;
+    margin-bottom: 55.5px;
+}
+`
+
+const HomeContainer = styled.div`
+font-family: 'Amatic SC', cursive;
+`
